a9b8597200f26b3e6386f8283bcdd5fbffaf6a39
Lucas Chaves <54557869+gothout@users.noreply.github.com>
2024-01-23 13:11:39 -0300

    Processar userBase.txt e limpar tela
    Criado uma função que processa o userBase.txt para poder exibir no menu e tambem criado um limpar tela

--- AgendaADA/src/Main.java
+++ AgendaADA/src/Main.java
@@ -2,6 +2,8 @@ import java.nio.file.Files; // Biblioteca para manipulação de arquivos
 import java.nio.file.Path; // Biblioteca para manipulação de caminhos de arquivos
 import java.nio.file.Paths; // Biblioteca para criação de objetos Path
 import java.io.IOException; // Verificação de erros na copilação
+import java.util.InputMismatchException; //Verifica erro de inputs
+import java.util.Scanner;
 
 
 public class Main {
@@ -11,25 +13,65 @@ public class Main {
         verificaNumberBase();
     }
 
-    public static void menuPrincipal(){
-        System.out.println("##################");
-        System.out.println("##### AGENDA #####");
-        System.out.println("##################");
-        getContatos();
-        System.out.println(">>>> Menu <<<<");
-        System.out.println("1 - Adicionar Contato");
-        System.out.println("2 - Remover Contato");
-        System.out.println("3 - Editar Contato");
-        System.out.println("4 - Sair");
+    public static void menuPrincipal() {
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+        do {
+            limparConsole();
+            System.out.println("##################");
+            System.out.println("##### AGENDA #####");
+            System.out.println("##################");
+            getContatos();
+            System.out.println(">>>> Menu <<<<");
+            System.out.println("1 - Adicionar Contato");
+            System.out.println("2 - Remover Contato");
+            System.out.println("3 - Editar Contato");
+            System.out.println("4 - Sair");
+
+            try {
+                System.out.print("Digite uma opção: ");
+                opcao = scanner.nextInt();
+
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida. Por favor, digite um número válido.");
+                scanner.nextLine(); // Limpa o buffer do scanner
+                opcao = 0; // Reinicializa a opção para evitar um loop infinito
+            }
+
+        } while (opcao <= 0 || opcao >= 5);
     }
 
     public static void getContatos(){
         System.out.println("");
         System.out.println(">>>> Contatos <<<<");
-        System.out.println("Em projeto");
+        System.out.println("Id | Nome ");
+        processarContatos("userBase.txt");
         System.out.println("");
     }
+    public static void processarContatos(String nomeArquivo) {
+        try {
+            // obtem o caminho do diretório do projeto
+            String diretorioProjeto = System.getProperty("user.dir");
 
+            // Cria um objeto Path com o caminho completo do arquivo
+            Path caminhoArquivo = Paths.get(diretorioProjeto, nomeArquivo);
+
+            // Lê todas as linhas do arquivo
+            Files.lines(caminhoArquivo).forEach(linha -> {
+                // Divide a linha em campos usando a vírgula como delimitador
+                String[] campos = linha.split(",");
+                // Verifica se a linha tem pelo menos três campos
+                if (campos.length >= 3) {
+                    // Concatena os três primeiros campos e imprime o resultado
+                    String resultado = campos[0] + " | " + campos[1] + " " + campos[2];
+                    System.out.println(resultado);
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
+    }
 
     public static void verificaUserBase() {
         // especifica o nome do arquivo que queremos verificar
@@ -80,4 +122,7 @@ public class Main {
             }
         }
     }
+
+    public static void limparConsole() { System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"); }
+
 }
